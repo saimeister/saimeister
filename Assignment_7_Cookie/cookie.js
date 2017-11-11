427d3d5753844441bb6317d8ec3f108d0d5fa8a6
saimeister <31781035+saimeister@users.noreply.github.com>
2017-11-11 00:29:47 -0500

    Add files via upload

---
+++ Assignment_7_Cookie/cookie.js
@@ -0,0 +1,9 @@
+document.cookie = "Username=Sai";
+document.cookie = "password=*****";
+document.cookie = "Location=Jersey City";
+document.cookie = "Occupation=Tester";
+document.cookie = "ZIP=07304";
+document.cookie = "Country=USA";
+function alertCookie() {
+  alert(document.cookie);
+}
